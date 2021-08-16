e749a03968c533094f50fb1d7cf8856f56e79d5a
Rahul Kolla <56570387+RahulKolla@users.noreply.github.com>
2021-08-16 00:09:12 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+    
+    public static void main(String[] args)
+    {
+        musicPlayerForm musicPlayerForm = new musicPlayerForm();
+        musicPlayerForm.setVisible(true);
+        musicPlayerForm.setResizable(false);
+        musicPlayerForm.setLocationRelativeTo(null);
+    }
+    
+}
